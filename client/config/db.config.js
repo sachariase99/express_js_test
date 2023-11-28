2a27d7ccda0b3ecf0c00fc4574c00300f9c88311
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2023-11-28 09:53:46 +0100

    s

---
+++ client/config/db.config.js
@@ -0,0 +1,16 @@
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const db = mysql.createConnection({
+    host: process.env.DBHOST,
+    database: process.env.DBNAME,
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    port: process.env.DBPORT,
+})
+
+db.connect()
+
+export default db
--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import db from './config/db.config.js'
 dotenv.config()
 
 const app = express()
@@ -9,10 +10,24 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/songs', (req, res) => {
+    db.query(`SELECT s.id, s.title, a.name FROM song s JOIN artist a ON s.artist_id = a.id`, (error, result) => {
+        res.json(result)
+    })
+})
+
+app.get('/songs/:id([0-9]*)', (req, res) => {
+    const { id } = req.params
+    const sql = `SELECT s.id, s.title, s.content, s.artist_id, a.name FROM song s JOIN artist a ON s.artist_id = a.id WHERE s.id = ${id}` 
+    db.query(sql, (error, result) => {
+        res.json(result)
+    })
+})
+
 app.get('*', (req, res) => {
     res.send('404 Page!')
 })
 
 app.listen(port, () => {
-    console.log(`Vi kører på port: ${port}`)
+    console.log(`Vi kører på port: http://localhost:${port}`)
 })
